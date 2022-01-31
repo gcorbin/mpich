ab2216140ecf84ebb005ba99afdcad4e44c96e12
Gregor Corbin <g.corbin@fz-juelich.de>
2022-01-31 11:10:09 +0100

    [scorep] Configure and run the test suite with Scorep
    - Add options to the runtests script
    - Set some environment variables for Score-P in runtests
    - Filter file for Score-P
    - Compiler wrappers
    - Configuration script

---
+++ test/mpi/scorep/configure-for-scorep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Execute this script from the main directory of mpi test, i.e. test/mpi/
+
+module load mpich/4.0
+module load scorep/7.0
+
+wdir=`pwd`/scorep/compiler-wrappers
+
+SCOREP_WRAPPER=off \
+./configure \
+CC=$wdir/scorep-mpi-only-gcc \
+CXX=$wdir/scorep-mpi-only-g++ \
+FC=$wdir/scorep-mpi-only-gfortran \
+F77=$wdir/scorep-mpi-only-gfortran \
+MPICC=$wdir/scorep-mpi-only-mpicc \
+MPICXX=$wdir/scorep-mpi-only-mpicxx \
+MPIFC=$wdir/scorep-mpi-only-mpif90 \
+MPIF77=$wdir/scorep-mpi-only-mpif77 \
+--prefix=`pwd`/install \
+--disable-dependency-tracking \
+--enable-fortran=f77,f90 \
+--disable-cxx \
+--enable-threads=funneled \
+--enable-strictmpi
